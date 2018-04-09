98378aed1bb1dfe6cc3f7c3e389c2dd076c607d9
Ryan Hamblin <ryanhca@gmail.com>
2018-04-08 21:36:33 -0600

    implements home button

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,8 +11,7 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    console.log(this.props.match);
-    const id = 1;
+    const { id } = this.props.match.params;
     this.fetchMovie(id);
   }
 
