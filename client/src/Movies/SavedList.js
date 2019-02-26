b91d3c8c39ef119d7223aa8b7c3afc8028d42f72
korynewton <korynewtondev@gmail.com>
2019-02-26 12:56:51 -0800

    Saved list is now individual Links to respective movie page

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -11,7 +11,7 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+          <Link to={`/movies/${movie.id}`} className="saved-movie">{movie.title}</Link>
         ))}
         <Link to="/" className="home-button">Home</Link>
       </div>
